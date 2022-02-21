17f6767df0c75360b11e6c63a34a35d736c16cb7
ViktorMensheninD <72730129+ViktorMensheninD@users.noreply.github.com>
2022-02-21 10:10:55 +0500

    Add files via upload

---
+++ vod-main/Voditel/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Voditel
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ZapExBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DOBDTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nasbtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void upBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DelDTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exitbtn_Click(object sender, RoutedEventArgs e)
+        {
+            enter enter = new enter();
+            enter.Show();
+            this.Close();
+        }
+    }
+}
---
+++ vod-main/Voditel/enter.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Voditel
+{
+    /// <summary>
+    /// Логика взаимодействия для enter.xaml
+    /// </summary>
+    public partial class enter : Window
+    {
+        public enter()
+        {
+            InitializeComponent();
+        }
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            Talon talon = new Talon();
+            talon.Show();
+        }
+
+        private void enterex_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
